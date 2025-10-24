40ed334930bb1dc7635bf19086bbb0f982bbc256
jjy12163 <lalalove0310@gmail.com>
2025-10-24 09:41:22 +0900

    Feat: 자동차 이름과 시도할 횟수 입력받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import racingcar.Controller.PlayRaceController;
+import racingcar.View.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        PlayRaceController playRaceController = new PlayRaceController(InputView.readInputName(), InputView.readInputNumber());
+
+
+
+
     }
 }
