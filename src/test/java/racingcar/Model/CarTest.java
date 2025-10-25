cc3a0d8d726a5f38fdc6113a2119b876d62b76c5
jjy12163 <lalalove0310@gmail.com>
2025-10-25 17:57:24 +0900

    Test: Car 객체 생성 테스트

---
+++ src/test/java/racingcar/Model/CarTest.java
@@ -0,0 +1,35 @@
+package racingcar.Model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarTest {
+
+    String name = "pobi";
+    Car car = new Car(name);
+
+    @Test
+    @DisplayName("getName() 테스트")
+    public void getNameTest() {
+        assertThat(car.getName()).isEqualTo(name);
+    }
+
+    @Test
+    @DisplayName("getPosition() 테스트")
+    public void getPositionTest() {
+        assertThat(car.getPosition()).isEqualTo("");
+    }
+
+    @Test
+    @DisplayName("setPosition() 테스트")
+    public void setPositionTest() {
+        car.setPosition(car.getPosition() + '-');
+        assertThat(car.getPosition()).isEqualTo("-");
+    }
+
+}
