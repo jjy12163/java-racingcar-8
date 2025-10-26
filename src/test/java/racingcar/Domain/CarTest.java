a245f2e97e0e940d5d4fdc107ed06b2d79e4ab55
jjy12163 <lalalove0310@gmail.com>
2025-10-26 15:52:45 +0900

    Test: Game의 최종 우승자 출력하는 테스트 케이스 추가

--- src/test/java/racingcar/Domain/CarTest.java
+++ src/test/java/racingcar/Domain/CarTest.java
@@ -11,19 +11,19 @@ public class CarTest {
     Car car = new Car(name);
 
     @Test
-    @DisplayName("getName() 테스트")
+    @DisplayName("자동차 이름 출력 테스트")
     public void getNameTest() {
         assertThat(car.getName()).isEqualTo(name);
     }
 
     @Test
-    @DisplayName("getPosition() 테스트")
+    @DisplayName("자동차 위치 출력 테스트")
     public void getPositionTest() {
         assertThat(car.getPosition()).isEqualTo("");
     }
 
     @Test
-    @DisplayName("setPosition() 테스트")
+    @DisplayName("자동차 위치 수정 테스트")
     public void setPositionTest() {
         car.setPosition(car.getPosition() + '-');
         assertThat(car.getPosition()).isEqualTo("-");
